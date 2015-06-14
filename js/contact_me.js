a0b32d6a0279435b4198726958804349d4f366ee
Mati Kochen <mati@timble.net>
2015-06-14 11:51:09 +0200

    add contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,14 +18,15 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                url: "http://formspree.io/info@qwackapp.com",
+                method: "POST",
                 data: {
                     name: name,
-                    phone: phone,
                     email: email,
-                    message: message
+                    message: message,
+                    _subject: "You've got Mail"
                 },
+                dataType: "json",
                 cache: false,
                 success: function() {
                     // Success message
